6ea9e6419ecac73a9c97d86d6e73e585ef477712
Sebastian Snoer <seb.snoer@gmail.com>
2020-10-04 16:00:23 +0200

    Initial Commit

--- src/main/java/dk/sebsa/amber/entity/Scene.java
+++
@@ -1,5 +0,0 @@
-package dk.sebsa.amber.entity;
-
-public class Scene {
-
-}
--- src/main/java/dk/sebsa/amber/math/Mathf.java
+++
@@ -1,9 +0,0 @@
-package dk.sebsa.amber.math;
-
-public class Mathf {
-	public static float clamp(float val, float min, float max) {
-		if(val<min) return min;
-		if(val>max) return max;
-		return val;
-	}
-}
